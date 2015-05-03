c19a2a7b76e6a97fee529d3e3907412b1eb24eb0
yashbajaj <yashbajajhyd@gmail.com>
2015-05-03 11:07:26 +0530

    All files pushed

---
+++ ranker.cpp
@@ -0,0 +1,32 @@
+#include <cstdio>
+#include <iostream>
+#include <set>
+#include <iterator>
+#include <vector>
+#include <cstring>
+#include <cstdlib>
+#include <algorithm>
+#include <string>
+
+
+
+using namespace std;
+
+
+struct tag
+{
+	int ground_truth;
+	double score;
+};
+
+typedef struct tag node;
+
+int main()
+{
+
+	node s;
+	FILE*fp = fopen("test_dataset.txt","w");
+	
+
+	return 0;
+}
